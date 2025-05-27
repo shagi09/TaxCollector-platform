4ada2b258ee899ebc77b38c28af3106266e87a24
Daniel Alemayehu <danialemayehu32@gmail.com>
2025-05-28 01:33:44 +0300

    payment fix

--- my-app/app/api/taxSystem/src/routes/payment.routes.js
+++ my-app/app/api/taxSystem/src/routes/payment.routes.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const paymentController = require('../controllers/payment.controller');
+const { payrollPayment, vatPayment,  profitTaxPayment,  } = require('../controllers/payment.controller');
 const { isLogin } = require('../middlewares/authMiddleware')
 
 // Payroll
-router.post('/payroll/:payrollMonthId', isLogin,  paymentController.payrollPayment);
-router.get('/transactions/:id', isLogin, paymentController.getPayrollReceipt);
+router.post('/payroll/:payrollMonthId', isLogin,  payrollPayment);
 
 // VAT
-router.post('/vat/:vatId', isLogin, paymentController.vatPayment);
-router.get('/transactions/vat/:id', isLogin, paymentController.getVatReceipt);
+router.post('/vat/:vatId', isLogin, vatPayment);
 
 // Profit Tax
-router.post('/profit-tax/:profitTaxId', isLogin,  paymentController.profitTaxPayment);
-router.get('/transactions/profit/:id', isLogin, paymentController.getProfitReceipt);
+router.post('/profit-tax/:profitTaxId', isLogin,  profitTaxPayment);
 
 module.exports = router;
